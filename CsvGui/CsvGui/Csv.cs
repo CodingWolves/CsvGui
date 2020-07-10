66f8363a5c0d6f6fcf5c07cfc64b92763e411002
idozahavy <idozahavy@users.noreply.github.com>
2020-07-10 17:13:40 +0300

    ReadFile Options

--- CsvGui/CsvGui/Csv.cs
+++ CsvGui/CsvGui/Csv.cs
@@ -10,35 +10,61 @@ namespace Csv
     {
         public static CsvForm ReadFile(string filename, bool hasHead)
         {
+            return ReadFile(filename, hasHead, 0, int.MaxValue, TimeSpan.MaxValue);
+        }
+        public static CsvForm ReadFile(string filename, bool hasHead, int rowOffset)
+        {
+            return ReadFile(filename, hasHead, rowOffset, int.MaxValue, TimeSpan.MaxValue);
+        }
+        public static CsvForm ReadFile(string filename, bool hasHead, int rowOffset, int rows)
+        {
+            return ReadFile(filename, hasHead, rowOffset, rows, TimeSpan.MaxValue);
+        }    
+        public static CsvForm ReadFile(string filename, bool hasHead, TimeSpan readTimeSpan)
+        {
+            return ReadFile(filename, hasHead, 0, int.MaxValue, readTimeSpan);
+        }
+
+        public static CsvForm ReadFile(string filename, bool hasHead, int rowOffset, int rows, TimeSpan readTimeSpan)
+        {
+            DateTime startTime = DateTime.Now;
             CsvForm form = new CsvForm();
             StreamReader stream = new StreamReader(filename);
+            int rowCount = 0;
             if (hasHead && !stream.EndOfStream)
             {
-                form.SetHeadRow(ReadRow(stream));
+                form.SetHeadRow(ReadRow(stream, ++rowCount));
+                rowOffset--;
             }
-            while (!stream.EndOfStream)
+            while (rowOffset > 0 && !stream.EndOfStream) // skip lines by rowOffset
             {
-                form.AddRow(ReadRow(stream));
+                stream.ReadLine();
+                rowOffset--;
+                rowCount++;
+            }
+            while (DateTime.Now.Subtract(startTime) < readTimeSpan && rows > rowCount && !stream.EndOfStream)
+            {
+                form.AddRow(ReadRow(stream, ++rowCount));
             }
             stream.Close();
             return form;
         }
 
-        public static CsvRow ReadRow(StreamReader stream)
+        public static CsvRow ReadRow(StreamReader stream, int rowIndex)
         {
             string line = stream.ReadLine();
-            //string line = ",123,";
-            CsvRow row = new CsvRow();
+            CsvRow row = new CsvRow(rowIndex);
+            int itemCount = 0;
             while (line.StartsWith(",")) // regex has a problem with empty items at start
             {
-                row.AddItem(new CsvString(String.Empty, row));
+                row.AddItem(new CsvString(String.Empty, row, ++itemCount));
                 line = line.Remove(0, 1);
             }
             MatchCollection itemMatches = Regex.Matches(line, "(?:^|,)(?=[^\"]|(\")?)\"?(?(1)[^\"]*|[^,\"]*)\"?(?=,|$)"); // Csv regex format            
             foreach (Match match in itemMatches)
             {
                 string itemValue = match.Value.StartsWith(",") ? match.Value.Remove(0, 1) : match.Value;
-                CsvString item = new CsvString(itemValue, row);
+                CsvString item = new CsvString(itemValue, row, ++itemCount);
                 row.AddItem(item);
             }
             return row;
